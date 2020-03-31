34115f87ef3351c88d5e9140a9d04dda0d621cc6
HzaZoro <1332314710@qq.com>
2020-03-31 14:47:10 +0800

    my first commit

---
+++ src/main/java/cn/pirateswang/personalshare/PersonalshareApplication.java
@@ -0,0 +1,13 @@
+package cn.pirateswang.personalshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalshareApplication.class, args);
+    }
+
+}
