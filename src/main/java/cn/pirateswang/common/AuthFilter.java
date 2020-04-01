402b89e7fdea2da8d086e1680d6cd6a6cb51388c
HzaZoro <1332314710@qq.com>
2020-04-01 09:23:05 +0800

    测试加filter

---
+++ src/main/java/cn/pirateswang/common/AuthFilter.java
@@ -0,0 +1,33 @@
+package cn.pirateswang.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@Order(2)
+@WebFilter(filterName = "authFilter",urlPatterns = "/*")
+@Component
+public class AuthFilter implements Filter {
+
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("============>AuthFilter-------->doFilter 【START】");
+
+
+
+
+
+
+
+
+        filterChain.doFilter(servletRequest,servletResponse);
+        log.info("<============AuthFilter<--------doFilter 【E N D】");
+    }
+}
